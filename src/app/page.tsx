23d053a87eab73678209ff7210f9a1758e53e2c7
greenymcgee <greenhouston@gmail.com>
2024-11-30 20:27:08 -0700

    #2 Adds Vitest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Image from 'next/image'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
@@ -5,7 +6,10 @@ export default function Home() {
   return (
     <>
       <SpeedInsights />
-      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <div
+        className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"
+        data-testid="page"
+      >
         <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
           <Image
             className="dark:invert"
