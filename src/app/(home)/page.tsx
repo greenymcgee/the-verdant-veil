b71c4209ce54756658559248dad115eb9c1b95cf
greenymcgee <greenhouston@gmail.com>
2024-12-16 16:36:08 -0700

    #94 Refactors Font Size Class Names

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <div className="container pt-44">
         <header className="bg-neutral-900 text-center text-white lg:bg-transparent">
           <Logo className="mb-6 inline-block text-[16rem] sm:text-[20rem] md:text-[24rem]" />
-          <h1 className="font-serif text-h-l sm:text-h-xl md:text-h-xxl">
+          <h1 className="md:text-heading-xxl sm:text-heading-xl text-heading-lg font-serif">
             Green Quest
           </h1>
         </header>
