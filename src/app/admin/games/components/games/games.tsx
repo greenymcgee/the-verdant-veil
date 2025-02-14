827761fd6ad39f176d42e1d407953f7811aef5bd
greenymcgee <greenhouston@gmail.com>
2025-02-13 19:38:05 -0700

    #283 Adds Fallback Data to Games Index

--- src/app/admin/games/components/games/games.tsx
+++ src/app/admin/games/components/games/games.tsx
@@ -1,13 +1,17 @@
+'use client'
 import React from 'react'
+import clsx from 'clsx'
 
-import { Button, LinkTo } from '@/components'
+import { Button, LinkTo, Spinner } from '@/components'
 import { ROUTES } from '@/constants'
+import { useGetGamesQuery } from '@/hooks/api'
 
-interface Props {
-  games: GameWithoutResources[]
-}
+export function Games() {
+  const { games, isLoading, isValidating } = useGetGamesQuery()
+  const skeleton = isLoading || isValidating
 
-export function Games({ games }: Props) {
+  if (isLoading && !games.length) return <Spinner className="py-32" size="lg" />
+
   return (
     <div className="overflow-x-auto">
       <table className="w-full min-w-96 text-left" data-testid="games">
@@ -24,12 +28,17 @@ export function Games({ games }: Props) {
               key={game.id}
             >
               <td className="px-4 py-3">
-                <LinkTo href={ROUTES.adminGame(game.slug)}>{game.name}</LinkTo>
+                <LinkTo
+                  className={clsx({ skeleton })}
+                  href={ROUTES.adminGame(game.slug)}
+                >
+                  {game.name}
+                </LinkTo>
               </td>
               <td className="px-4 py-3">
                 <LinkTo
                   aria-label={`Edit ${game.name}`}
-                  className="mr-4"
+                  className={clsx('mr-4', { skeleton })}
                   href={ROUTES.adminEditGame(game.slug)}
                   leftIcon="edit"
                   size="sm"
@@ -37,6 +46,7 @@ export function Games({ games }: Props) {
                 />
                 <Button
                   aria-label={`Delete ${game.name}`}
+                  className={clsx({ skeleton })}
                   leftIcon="trash"
                   size="sm"
                   theme="danger"
