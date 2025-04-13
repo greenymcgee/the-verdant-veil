dd7a57b3f0ada51ca425491903218b87f8146de0
greenymcgee <greenhouston@gmail.com>
2025-04-13 14:30:05 -0600

    #531 Replaces body-scroll-lock

--- src/hooks/useModalDialogToggle.ts
+++ src/hooks/useModalDialogToggle.ts
@@ -1,15 +1,8 @@
 'use client'
 import { RefObject, useCallback } from 'react'
 
-import { usePageContext } from '@/context'
 import { toggleModalDialog } from '@/utils'
 
 export function useModalDialogToggle(ref: RefObject<HTMLDialogElement | null>) {
-  const {
-    userAgent: { isIOSDevice },
-  } = usePageContext()
-
-  return useCallback(() => {
-    toggleModalDialog(ref.current, !isIOSDevice)
-  }, [isIOSDevice, ref])
+  return useCallback(() => toggleModalDialog(ref.current), [ref])
 }
